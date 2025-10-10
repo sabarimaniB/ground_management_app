fc35f2ee0991db762d7cc1878fa4295905df2425
sabarimaniB <sabarimani.b2022eee@sece.ac.in>
2025-10-11 03:47:17 +0530

    asaddad

--- Backend/routes/bookingroute.js
+++ Backend/routes/bookingroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { createBooking, getUserBookings, getAllBookings, deleteBooking } = require('../controllers/bookingcontroller');
+const { createBooking, getUserBookings, getAllBookings, deleteBooking } = require('../controllers/bookingController');
 const auth = require('../middlewares/auth')
 
 // ✅ Correct usage
